a06ba0d91fed8f64cdc39d56b32045f7324c6256
krwojtowicz <krwojtowiczz@gmail.com>
2023-03-11 21:41:20 +0100

    task 3,4 done task 5 started

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(base.Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return base.GetLetterGrade(averageGrade);
+        }
+    }
+}
